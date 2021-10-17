091cad22c9713326f1f144bca073c68b38debd29
Swapnil Kole <swapnilkole7500@gmail.com>
2021-10-18 00:07:44 +0530

    sql commands finalized

--- sqlCommands.sql
+++ sqlCommands.sql
@@ -43,9 +43,9 @@ INSERT INTO `customer` (`customer_id`, `Name`, `PhnNo`) VALUES
 (503, 'rahul', 9207049981),
 (504, 'aditi', 9658077043),
 (505, 'sakshi', 3424256424),
-(507, 'sruti', 6370126201),
-(510, 'harish', 2045613378),
-(511, 'sana', 78945661200);
+(506, 'sruti', 6370126201),
+(507, 'harish', 2045613378),
+(508, 'sana', 78945661200);
 
 -- --------------------------------------------------------
 
@@ -59,31 +59,31 @@ CREATE TABLE `orders` (
   `qty` bigint(20) NOT NULL,
   `price` double NOT NULL,
   `product_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL
+  `customer_id` int(11) NOT NULL,
+  'bill_id' int(11) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `order_date`, `qty`, `price`, `product_id`, `customer_id`) VALUES
-(1, '2021-01-27', 15, 40, 1, 501),
-(2, '2021-01-22', 10, 32, 2, 502),
-(3, '2021-01-27', 20, 42, 3, 503),
-(4, '2021-01-21', 27, 21, 4, 504),
-(5, '2021-02-03', 10, 45, 1, 504),
-(6, '2021-03-10', 7, 35, 2, 507),
-(7, '2021-02-09', 12, 45, 3, 502),
-(8, '2021-02-17', 10, 22, 4, 502),
-(9, '2021-03-10', 20, 45, 1, 502),
-(10, '2021-04-14', 15, 33, 5, 502),
-(11, '2021-05-12', 6, 22, 4, 507),
-(12, '2021-06-16', 10, 43, 3, 507),
-(13, '2021-07-15', 15, 45, 1, 505),
-(14, '2021-08-18', 7, 31, 2, 507),
-(15, '2021-07-14', 10, 41, 3, 505),
-(16, '2021-07-28', 4, 25, 4, 505),
-(407, '2021-08-09', 10, 44, 1, 507);
+INSERT INTO `orders` (`order_id`, `order_date`, `qty`, `price`, `product_id`, `customer_id`, `bill_id`) VALUES
+(1, '2021-01-27', 15, 40, 1, 501, 1),
+(2, '2021-01-22', 10, 32, 2, 502, 1),
+(3, '2021-01-27', 20, 42, 3, 503, 1),
+(4, '2021-01-21', 27, 21, 4, 504, 2),
+(5, '2021-02-03', 10, 45, 1, 504, 2),
+(6, '2021-03-10', 7, 35, 2, 507, 2),
+(7, '2021-02-09', 12, 45, 3, 502, 2),
+(8, '2021-02-17', 10, 22, 4, 502, 2),
+(9, '2021-03-10', 20, 45, 1, 502, 2),
+(10, '2021-04-14', 15, 33, 5, 502, 3),
+(11, '2021-05-12', 6, 22, 4, 507, 3),
+(12, '2021-06-16', 10, 43, 3, 507, 3),
+(13, '2021-07-15', 15, 45, 1, 505, 4),
+(14, '2021-08-18', 7, 31, 2, 507, 4),
+(15, '2021-07-14', 10, 41, 3, 505, 4),
+(16, '2021-07-28', 4, 25, 4, 505, 4);
 
 -- --------------------------------------------------------
 
@@ -134,7 +134,7 @@ INSERT INTO `supplier` (`supplier_id`, `name`, `phn`) VALUES
 (104, 'Fresh Point', 5035559871),
 (105, 'Cradle', 5035558861),
 (106, 'The Lightman Group', 5035559891),
-(108, 'Sassafaras', 5052461374);
+(107, 'Sassafaras', 5052461374);
 
 --
 -- Indexes for dumped tables
@@ -146,7 +146,7 @@ INSERT INTO `supplier` (`supplier_id`, `name`, `phn`) VALUES
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`customer_id`),
   ADD KEY `PhnNo` (`PhnNo`),
-  ADD KEY `PhnNo_2` (`PhnNo`);
+  -- ADD KEY `PhnNo_2` (`PhnNo`);
 
 --
 -- Indexes for table `orders`
@@ -155,6 +155,7 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`),
   ADD KEY `customer_id` (`customer_id`),
   ADD KEY `product_id` (`product_id`);
+  ADD KEY  `bill_id` (`bill_id`);
 
 --
 -- Indexes for table `products`
@@ -177,25 +178,24 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=512;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=509;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=408;
-
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
-  MODIFY `supplier_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `supplier_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
 -- Constraints for dumped tables
